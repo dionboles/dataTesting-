51ccba0afa69851cb712ade31bef8fab16190db3
Dion Boles <dion.boles@gmail.com>
2019-06-22 19:32:15 -0400

    Update testing.py

--- testing_Ben's_code/testing.py
+++ testing_Ben's_code/testing.py
@@ -1,6 +1,6 @@
 import os
 print( "\n\n\t\t\t*** INSTALLING PIP WHEELS ***\n")
-names = [name for name in os.listdir("/srv/sisa/repo/pip/")if name.endswith(".whl")]
+names = [name for name in os.listdir("/srv/pips/repo/pip/")if name.endswith(".whl")]
 
 for i in names:
-    os.system("pip install /srv/sisa/repo/pip/"+i)
+    os.system("pip install /srv/pips/repo/pip/"+i)
